27d238218d08c65ac3e52bcfe3a18db6ee0ae1ea
NAZIM <nazimmds025@gmail.com>
2024-07-10 09:11:41 +0530

    cv

--- server/server.js
+++ server/server.js
@@ -18,22 +18,8 @@ connectDB();
 const app = express();
 
 
-// CORS configuration
-const allowedOrigins = [
-  'https://ecommerce-client-red.vercel.app',
-  'http://localhost:3000', // add other allowed origins here
-];
 //Middlewares
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true); // allow requests with no origin (like mobile apps, curl, etc)
-    if (allowedOrigins.indexOf(origin) === -1) {
-      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }
-    return callback(null, true);
-  }
-}));
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
@@ -43,16 +29,16 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //REST Api
-app.get("/", (req, res) => {
-  res.send({
-    message: "Welcome to Ecommerce",
-  });
-});
-
 // app.get("/", (req, res) => {
-//   app.use(express.static(path.resolve(__dirname, "client", "build")));
-//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   res.send({
+//     message: "Welcome to Ecommerce",
 //   });
+// });
+
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 
 //PORT
 const PORT = process.env.PORT || 8080;

8d7e4ad05c935dc894fb4fd93269861d45a215a2
NAZIM <nazimmds025@gmail.com>
2024-07-10 11:17:07 +0530

    fileurl

--- server/server.js
+++ server/server.js
@@ -7,12 +7,17 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from "url";
 
 //configure ENv
 dotenv.config();
 
 //DB config
 connectDB();
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //REST object
 const app = express();
@@ -22,6 +27,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, './client/build')))
 
 //route
 app.use("/api/v1/auth", authRoutes);
@@ -29,16 +35,20 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //REST Api
+app.use("*", function(req,res){
+  res.sendFile(path.join(__dirname, "./client/build/index.html"))
+})
+
 // app.get("/", (req, res) => {
 //   res.send({
 //     message: "Welcome to Ecommerce",
 //   });
 // });
 
-app.get("/", (req, res) => {
-  app.use(express.static(path.resolve(__dirname, "client", "build")));
-  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
+// app.get("/", (req, res) => {
+//   app.use(express.static(path.resolve(__dirname, "client", "build")));
+//   res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
 
 //PORT
 const PORT = process.env.PORT || 8080;
